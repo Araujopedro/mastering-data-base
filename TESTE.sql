9974764f8b81ba399fcfad5edf915672055de763
Pedro Araujo <91104140+Araujopedro@users.noreply.github.com>
2024-08-22 21:31:23 -0300

    aula 2
    uso de IF  e else if com filtros

--- TESTE.sql
+++ TESTE.sql
@@ -1,20 +1,49 @@
-BEGIN
-    FOR X IN (
-    SELECT
-        TABLE_NAME
+SET SERVEROUTPUT ON;
+DECLARE
+    genero VARCHAR2(30) := '&digiteUmvalor';
+    BEGIN 
+        IF  UPPER(genero) = 'M' THEN
+            dbms_output.put_line ('O genero digitado é masculino');
+            
+        ELSIF Upper(genero) = 'F' THEN
+             dbms_output.put_line ('O genero digitado é femenino');
+             
+        ELSE
+             dbms_output.put_line ('NÃO ENCONTRADO');
+             
+          END IF;
+          END;
+------------------------------------------------------------------------------------------------------------
 
-    FROM
-        user_tables
-    WHERE
-        table_name = 'NOVO PEDIDO'
-        
-        )LOOP
+
+
+DECLARE
+        NUMERO NUMBER := '&DIGITEOVALOR';
+    BEGIN
     
-    EXECUTE IMMEDIATE 'DROP TABLE' 
-                        ||X.TABLE_NAME
-                        || 'CASDADE CONSTRAINTS';
-                               
-END LOOP;
-END;
-
+    IF MOD (NUMERO ,2) = 0 THEN
+        dbms_output.put_line ('par');
+    
+    ELSIF MOD (numero,2) != 0 THEN
+         dbms_output.put_line ('IMPAR');
+         
+    ELSE
+         dbms_output.put_line ('par');
+         
+    END IF;
+    
+    END;
+    
+         
+    
+    
+    
+        
+        
+             
+    
+             
+             
+        
+        
         
